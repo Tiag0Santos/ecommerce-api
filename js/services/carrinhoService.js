e0a720edba7968b844ac686c1687bc3e3d4bbd6f
Tiag0Santos <titaods@gmail.com>
2026-03-23 10:24:27 -0300

    feat: lógica do carrinho

--- js/services/carrinhoService.js
+++ js/services/carrinhoService.js
@@ -1,6 +1,10 @@
 let carrinho =
 JSON.parse(localStorage.getItem("carrinho")) || []
 
+export function obterCarrinho(){
+    return carrinho
+}
+
 export function adicionarCarrinho(produto){
 
     const item = carrinho.find(p => p.id === produto.id)
@@ -17,12 +21,31 @@ export function adicionarCarrinho(produto){
     salvarCarrinho()
 }
 
+export function aumentarQuantidade(id){
+    const item = carrinho.find(p => p.id === id)
+
+    if(item){
+        item.quantidade++
+        salvarCarrinho()
+    }
+}
+
+export function diminuirQuantidade(id){
+    const item = carrinho.find(p => p.id === id)
+
+    if(item && item.quantidade > 1){
+        item.quantidade--
+        salvarCarrinho()
+    }
+}
+
+export function removerProduto(){
+    carrinho = carrinho.filter(p => p.id !== id)
+
+    salvarCarrinho()
+}
+
 function salvarCarrinho(){
 
     localStorage.setItem("carrinho",JSON.stringify(carrinho))
-}
-
-export function obterCarrinho(){
-
-    return carrinho
 }
